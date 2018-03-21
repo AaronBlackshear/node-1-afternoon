1fab83c2b8fe034009f54829a3d47cb2c2f9b21e
Aaron Blackshear <theshiftybapple@gmail.com>
2018-03-21 14:44:39 -0500

    Finished Steps 1-5

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const messages = require('./controllers.messages.controller');
+app.use(bodyParser.json());
+const port = 3000;
+
+    const messageBaseUrl = "/api/messages";
+    app.post(messageBaseUrl, messages.create);
+
+    app.get(messageBaseUrl, messages.post);
+
+    app.put(`${messageBaseUrl}/:id`, messages.update);
+
+    app.delete(`${messageBaseUrl}/:id`, messages.delete);
+
+app.listen(port, () => `Listening to port ${port}`);
